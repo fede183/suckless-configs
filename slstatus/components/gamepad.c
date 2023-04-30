247ae556d24acdc5213b5a73a3a4d509eefc2ce0
fede183 <fede.183@hotmail.com>
2023-04-30 15:29:49 -0300

    gamepad

--- slstatus/components/gamepad.c
+++ slstatus/components/gamepad.c
@@ -12,7 +12,7 @@
 
 const char *gamepad_connect(const char *unused)
 {
-	char *bluetooth_list = (char *)run_command("timeout 2 bluetoothctl scan on");
+	char *bluetooth_list = (char *)run_command("timeout 5 bluetoothctl scan on");
 	char *gamepad_id = "8C:41:F2:07:02:3F";
 
     if (strstr(bluetooth_list, gamepad_id) != NULL) {
