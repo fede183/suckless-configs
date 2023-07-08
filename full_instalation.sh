f5c142ce7856a940ee9e85f8a5ea748dc504b487
fede183 <fede.183@hotmail.com>
2023-07-08 12:15:14 -0300

    script updated

--- full_instalation.sh
+++ full_instalation.sh
@@ -1,5 +1,7 @@
-apt install build-essential libpam0g-dev libxcb-xkb-dev
+# Installing dependencies
+xargs -rxa dependencies.txt -- sudo apt-get install -y --;
 
+# Installing suckless programs
 modules_to_install='dwm dmenu st slstatus'
 
 for module in $modules_to_install; do
@@ -17,3 +19,9 @@ for module in $modules_to_install; do
     sudo make clean;
     cd ..;
 done
+
+# Finish dwm instalation
+apt-get install dwm
+cp /usr/share/xsessions/dwm.desktop{,.bak}
+apt-get purge dwm
+mv /usr/share/xsessions/dwm.desktop{.bak,}
