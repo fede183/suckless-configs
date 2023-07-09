b74d0ef51b3af62bbfbc55e9198d92506b9fe6a1
fede183 <fede.183@hotmail.com>
2023-07-09 19:21:17 -0300

    some changes

--- full_instalation.sh
+++ full_instalation.sh
@@ -5,7 +5,7 @@ xargs -rxa dependencies.txt -- sudo apt-get install -y --;
 git apply patches/*;
 
 # Installing suckless programs
-modules_to_install='dwm dmenu st slstatus'
+modules_to_install='dwm dmenu st slstatus libxft-bgra'
 
 for module in $modules_to_install; do
     echo $module
