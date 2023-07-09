464182a97289446067f9bfb8a7dd4a377ef34aae
fede183 <fede.183@hotmail.com>
2023-07-09 18:55:44 -0300

    volume patch added for slstatus

--- full_instalation.sh
+++ full_instalation.sh
@@ -10,7 +10,7 @@ modules_to_install='dwm dmenu st slstatus'
 for module in $modules_to_install; do
     echo $module
     cd $module;
-    rm config.h;
+    sudo rm config.h;
     sudo make clean install;
     cd ..;
 done
@@ -18,7 +18,7 @@ done
 for module in $modules_to_install; do
     echo $module
     cd $module;
-    rm config.h;
+    sudo rm config.h;
     sudo make clean;
     git checkout -- .;
     cd ..;
