900dbf05e470d7394e0848e4866c943830c717d5
fede183 <fede.183@hotmail.com>
2023-07-09 18:54:30 -0300

    add patches to full installation script

--- full_instalation.sh
+++ full_instalation.sh
@@ -1,6 +1,9 @@
 # Installing dependencies
 xargs -rxa dependencies.txt -- sudo apt-get install -y --;
 
+# Applying patches
+git apply patches/*;
+
 # Installing suckless programs
 modules_to_install='dwm dmenu st slstatus'
 
@@ -17,6 +20,7 @@ for module in $modules_to_install; do
     cd $module;
     rm config.h;
     sudo make clean;
+    git checkout -- .;
     cd ..;
 done
 
