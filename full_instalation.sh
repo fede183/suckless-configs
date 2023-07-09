64db0ab830c471826abe2af7283081a4881e6b3e
fede183 <fede.183@hotmail.com>
2023-07-09 19:27:28 -0300

    volume patch added for slstatus

--- full_instalation.sh
+++ full_instalation.sh
@@ -5,7 +5,7 @@ xargs -rxa dependencies.txt -- sudo apt-get install -y --;
 git apply patches/*;
 
 # Installing suckless programs
-modules_to_install='dwm dmenu st slstatus libxft-bgra'
+modules_to_install='dwm dmenu st slstatus'
 
 for module in $modules_to_install; do
     echo $module
@@ -29,3 +29,14 @@ sudo apt-get install dwm -y
 sudo cp /usr/share/xsessions/dwm.desktop{,.bak}
 sudo apt-get purge dwm -y
 sudo mv /usr/share/xsessions/dwm.desktop{.bak,}
+
+# Fix the fonts
+cd libxft-bgra;
+sh autogen.sh --sysconfdir=/etc --prefix=/usr --mandir=/usr/share/man;
+sudo make install;
+cd ..;
+
+mkdir ~/.local/share/fonts;
+unzip JetBrainsMono.zip ~/.local/share/fonts/;
+unzip Hack.zip ~/.local/share/fonts/;
+sudo fc-cache -fv;
