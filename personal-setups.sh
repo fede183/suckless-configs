0b8dfdcab25a14f0acf38b1d94d60a2329c621eb
fede183 <fede.183@hotmail.com>
2023-07-29 12:54:58 -0300

    gamepad

--- personal-setups.sh
+++ personal-setups.sh
@@ -1,6 +1,2 @@
-# Creating alias
-unalias connect_to_gamepad;
-alias connect_to_gamepad='gamepad_id='8C:41:F2:07:02:3F';
-timeout 5 bluetoothctl scan on;
-bluetoothctl pair $gamepad_id;
-bluetoothctl connect $gamepad_id;';
+# Gamepad script
+sudo cp gamepad-connect.sh /usr/local/bin/;
